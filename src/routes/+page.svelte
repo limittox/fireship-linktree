b6a46fe4565a383302d074c3df0e511fd1345c23
Yathu Arul <yathu@outlook.com>
2024-07-13 00:30:16 +1000

    Added tailwind and daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<input type="text" placeholder="Type something" class="input input-bordered w-full max-w-xs"/>
